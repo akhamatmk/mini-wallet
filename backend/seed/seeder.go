c928564923f2d2c6eece7b16fbb06856a87d11de
akhamat_mhealthbank <akhamat.mk@mhealthbank.com>
2020-06-20 20:03:32 +0700

    commit this

--- backend/seed/seeder.go
+++ backend/seed/seeder.go
@@ -3,8 +3,8 @@ package seed
 import (
 	"log"
 
+	"github.com/akhamatvarokah/mini-wallet/backend/models"
 	"github.com/jinzhu/gorm"
-	"github.com/victorsteven/fullstack/api/models"
 )
 
 var users = []models.User{
